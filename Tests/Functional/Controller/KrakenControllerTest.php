801a02c4f80e3e238cf68265fbf48d4f9f7475af
Gerhard Boden <gerhard.boden@gmail.com>
2018-05-13 15:40:14 +0200

    TASK: Add Unit and Functional Tests

---
+++ Tests/Functional/Controller/KrakenControllerTest.php
@@ -0,0 +1,73 @@
+<?php
+namespace GesagtGetan\KrakenOptimizer\Tests\Functional\Controller;
+
+use GesagtGetan\KrakenOptimizer\Service\KrakenService;
+use TYPO3\Flow\Tests\FunctionalTestCase;
+
+class KrakenControllerTest extends FunctionalTestCase
+{
+    protected static $testablePersistenceEnabled = true;
+
+    const REPLACE_ACTION_URI = '/optimizeImage';
+
+    /**
+     * @var KrakenService
+     */
+    protected $krakenService;
+
+    public function setUp()
+    {
+        parent::setUp();
+        $this->krakenService = $this->objectManager->get(KrakenService::class);
+    }
+
+    /**
+     * Controller should throw exception if verification token is missing in request.
+     *
+     * @test
+     */
+    public function missingVerificationTokenThrowsException()
+    {
+        $payload = [
+            'success' => 'true',
+            'file_name' => 'e6f36746ccba42c288acf906e636bb278eaeb7e8'
+        ];
+
+        $response = $this->browser->request('http://localhost' . self::REPLACE_ACTION_URI, 'POST', $payload);
+        $this->assertEquals('1524665601', $response->getHeader('X-Flow-ExceptionCode'));
+    }
+
+    /**
+     * Controller should throw exception if verification token is invalid.
+     *
+     * @test
+     */
+    public function arbitraryVerificationTokenThrowsException()
+    {
+        $payload = [
+            'success' => 'true',
+            'file_name' => 'e6f36746ccba42c288acf906e636bb278eaeb7e8',
+            'verificationToken' => '$2c$03$vb.Vrkt3BddbmR.4yc6gJOfG5p.5BEeUcUIvMCvkQQe43de6S98ni'
+        ];
+
+        $response = $this->browser->request('http://localhost' . self::REPLACE_ACTION_URI, 'POST', $payload);
+        $this->assertEquals('1524665601', $response->getHeader('X-Flow-ExceptionCode'));
+    }
+
+    /**
+     * No exception should be thrown if payload passes checks.
+     *
+     * @test
+     */
+    public function controllerShouldReturnNullIfPayloadIsValid()
+    {
+        $payload = [
+            'success' => 'true',
+            'file_name' => 'e6f36746ccba42c288acf906e636bb278eaeb7e8'
+        ];
+        $payload['verificationToken'] = $this->krakenService->createToken($payload['file_name']);
+
+        $response = $this->browser->request('http://localhost' . self::REPLACE_ACTION_URI, 'POST', $payload);
+        $this->assertEquals('null', $response->getContent());
+    }
+}
---
+++ Tests/Functional/Service/KrakenServiceTest.php
@@ -0,0 +1,49 @@
+<?php
+namespace GesagtGetan\KrakenOptimizer\Tests\Functional\Service;
+
+use TYPO3\Flow\Resource\Resource;
+use GesagtGetan\KrakenOptimizer\Service\KrakenService;
+use TYPO3\Flow\Tests\FunctionalTestCase;
+use TYPO3\Flow\Resource\ResourceManager;
+
+class KrakenServiceTest extends FunctionalTestCase
+{
+    /**
+     * @var ResourceManager
+     */
+    protected $resourceManager;
+
+    /**
+     * @var boolean
+     */
+    protected static $testablePersistenceEnabled = true;
+
+    /**
+     * @var KrakenService
+     */
+    protected $krakenService;
+
+    /**
+     * @var Resource
+     */
+    protected $testResource;
+
+    public function setUp()
+    {
+        parent::setUp();
+        $this->krakenService = $this->objectManager->get(KrakenService::class);
+        $this->resourceManager = $this->objectManager->get(ResourceManager::class);
+        $svgContent = '<svg xmlns="http://www.w3.org/2000/svg" height="30" width="200"> <text x="0" y="15" fill="red">Test SVG</text></svg>';
+        $this->testResource = $this->resourceManager->importResourceFromContent($svgContent, 'svg1526213193.svg');
+    }
+
+    /**
+     * @test
+     */
+    public function apiRequestWithArbitraryResourceReturnsSuccessfulResponse()
+    {
+        $response = json_decode($this->krakenService->requestOptimizedResource($this->testResource, ['wait' => true]), true);
+
+        $this->assertTrue($response['success']);
+    }
+}
